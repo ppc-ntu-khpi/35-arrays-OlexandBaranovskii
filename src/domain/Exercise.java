b0cdb22b793af6dd607631de9520034ccbcfe527
OlexandBaranovskii <72404610+OlexandBaranovskii@users.noreply.github.com>
2021-02-10 22:34:18 +0300

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,20 @@
 package domain;
-
+/**
+ * Class that represent an exercise
+ * @author DevShade
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    
+    /**
+     * Method that fills the array
+     * @param array integer array
+     */
+    public static void fillArray(int[] array){
+        int a = (int) Math.ceil(array.length / 2.0) - 1;
+        
+        for (int i = 0; i < a; i++) {
+            array[i] = a - i;
+            array[array.length - i - 1] = array[i];
+        }
     }
 }
